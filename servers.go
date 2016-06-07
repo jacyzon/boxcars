ecdb4749c5bd9ce49a5b7bc634afee8a946f68d8
Chen Yi Zhang <jz@jacyzon.com>
2016-06-08 00:47:26 +0800

    not adding trailing slash if request path is empty

--- servers.go
+++ servers.go
@@ -3,15 +3,16 @@ package boxcars
 import (
 	"fmt"
 	"net/http"
-	"net/http/httputil"
 	"net/url"
+	"net/http/httputil"
+	"strings"
 	"regexp"
 )
 
 func ReverseProxyServer(uri string) http.Handler {
 	debug("Returning a reverse proxy server for %s.", uri)
 	dest, _ := url.Parse(addProtocol(uri))
-	return httputil.NewSingleHostReverseProxy(dest)
+	return NewSingleHostReverseProxy(dest)
 }
 
 func newStaticServer(uri string, hasCustom404 bool, custom404 string) http.Handler {
@@ -30,4 +31,34 @@ func addProtocol(url string) string {
 	}
 
 	return url
+}
+
+func singleJoiningSlashWithoutTrailing(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	bempty := len(b) == 0
+	switch {
+	case bempty:
+		return a
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+func NewSingleHostReverseProxy(target *url.URL) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlashWithoutTrailing(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+	}
+	return &httputil.ReverseProxy{Director: director}
 }
